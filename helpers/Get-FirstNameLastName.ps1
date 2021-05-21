9b0d8d12a4ca975612288e76f5683f17bf1bc492
Guerrero <dan.guerrero@terumobct.com>
2021-05-21 09:49:45 -0600

    Get-FirstNameLastName.ps1 commit

---
+++ helpers/Get-FirstNameLastName.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+    Given a name string, extract the first and last name
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    $name: string "FirstName LastName"
+.OUTPUTS
+    array object with firstName in the 0 index
+    lastName in the 1 index
+.NOTES
+    General notes
+#>
+function Get-FirstNameLastName {
+    
+    param (
+        [string]
+        $name
+    )
+
+    # can't process empty name
+    if ("" -eq $name) {
+        return $null
+    }
+
+    $firstNameLastName = @()
+
+    $fullName = $name.Split(' ')
+    if ($fullName.Length -eq 2)
+    {
+        $firstNameLastName += $fullName[0]
+        $firstNameLastName += $fullName[1]
+    }
+
+    # return array with firstName in the 0 index
+    # and lastName in the 1 index
+    return $firstNameLastName
+}
