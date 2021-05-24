0cf972051041e6ec703e5b6d98160e1502e593b7
Guerrero <dan.guerrero@terumobct.com>
2021-05-24 16:59:26 -0600

    Get-AllGitHubUsers.ps1

---
+++ Get-AllGitHubUsers.ps1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+    Generates a list of all github users and exports the list to GitHubUsers.csv
+.DESCRIPTION
+    This is a wrapper to GitHubUsers.ps1.
+
+    The generated CSV file can be imported into an Excel spreadsheet and filtered accordingly
+.EXAMPLE
+    PS C:\> ./Get-AllGitHubUsers.ps1
+.INPUTS
+    none
+.OUTPUTS
+    CSV file with the following columns:
+    FullName    Email   bctLogin    gitHubLogin disabled
+
+    where
+        FullName: LastName, FirstName (if known)
+        bctLogin/gitHubLogin: login name of respective domain
+        disabled: 'X' indicates that the user's AD account has been disabled
+.NOTES
+    General notes
+#>
+./helpers/Get-GitHubUsers.ps1 | Select-Object | Export-CSV -NoTypeInformation -Path ./GitHubUsers.csv
